8d35d0d467c5a42d6b9167dcd59e0d20febb6586
MarinoLinic <84214511+MarinoLinic@users.noreply.github.com>
2022-07-22 11:39:59 +0200

    test navigation, CSS, Prettier setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,35 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { useState } from 'react'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }: AppProps) {
-	const [klemens, setklemens] = useState("klemens")
-	console.log(useState("klemens")[0])
 	return (
-		<div>
-			<button onClick={() => setklemens(klemens+"s")}>Click me!</button>
-			{useState("klemens")[0]}
-			{klemens}
+		<>
+			<div className="navigation text-white">
+				<div className="nav-title pull-left">
+					<Link href="/">Naval Wars</Link>
+				</div>
+				<ul className="pull-right">
+					<li className="nav-button">
+						<Link href="/klemsen">Play</Link>
+					</li>
+
+					<li className="nav-button">
+						<Link href="/klemsen">User profile</Link>
+					</li>
+
+					<li className="nav-button">
+						<Link href="/klemsen">About</Link>
+					</li>
+
+					<li className="nav-button">
+						<Link href="/klemsen">Log out</Link>
+					</li>
+				</ul>
+			</div>
+
 			<Component {...pageProps} />
-		</div>
+		</>
 	)
 }
 
--- pages/klemsen.tsx
+++ pages/klemsen.tsx
@@ -1,9 +1,17 @@
-import * as React from 'react';
+import * as React from 'react'
+import { useState } from 'react'
 
 function Cornelius() {
-    return (
-        <p>Klemsen er en sølvfisk</p>
-    )
+	const [klemens, setklemens] = useState('klemens')
+	console.log(useState('klemens')[0])
+	return (
+		<div>
+			<p>Klemsen er en sølvfisk</p>
+			<button onClick={() => setklemens(klemens + 's')}>Click me!</button>
+			{useState('klemens')[0]}
+			{klemens}
+		</div>
+	)
 }
 
 export default Cornelius
