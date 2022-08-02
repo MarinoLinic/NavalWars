1293a1ad7587462f4b3fb8153a37ab7c3ed4960f
MarinoLinic <84214511+MarinoLinic@users.noreply.github.com>
2022-08-02 23:39:13 +0200

    Profile updates

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,13 +1,16 @@
 import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0'
-import { useState } from 'react'
-import { useRef } from 'react'
+import { useState, useRef } from 'react'
 
 const Profile = () => {
 	const { user, error, isLoading } = useUser()
 	const [profileName, changeProfileName] = useState(user && user.name)
 	const [characterEditIsShown, setCharacterEditIsShown] = useState(false)
-	const [charNum, changeCharNum] = useState(0)
-	const [characterName, changeCharacterName] = useState('Character')
+	const [charNum, changeCharNum] = useState(Math.floor(Math.random() * 4))
+	const [characterName, changeCharacterName] = useState(
+		user && user.name + "'s character"
+	)
+	const [charList, setCharList] = useState([]) as any
+
 	const inputRef = useRef(null) as any
 
 	let chars = [
@@ -17,23 +20,38 @@ const Profile = () => {
 		'/avatars/4.png'
 	]
 
-	const charNumChange = (charDirection: any) => {
-		if (charDirection && charNum < chars.length - 1) changeCharNum(charNum + 1)
-		else if (!charDirection && charNum > 0) changeCharNum(charNum - 1)
-
-		if (charDirection && charNum === chars.length - 1) changeCharNum(0)
-		else if (!charDirection && charNum === 0) changeCharNum(chars.length - 1)
-	}
-
-	let profilePicture = (user && user.picture) || '/sailing_ship.png' // if the gmail profile picture is not available, use the default; nullish coalescing probably unnecessary
+	// var charList: any = []
+	console.log(charList)
 
 	if (isLoading) return <div>Loading...</div>
 	if (error) return <div>{error.message}</div>
+
+	const charNumChange = (charForward: boolean) => {
+		if (charForward && charNum < chars.length - 1) changeCharNum(charNum + 1)
+		else if (!charForward && charNum > 0) changeCharNum(charNum - 1)
+
+		if (charForward && charNum === chars.length - 1) changeCharNum(0)
+		else if (!charForward && charNum === 0) changeCharNum(chars.length - 1)
+	}
+
+	let profilePicture = (user && user.picture) || '/sailing_ship.png' // if the gmail profile picture is not available, use the default; nullish coalescing probably unnecessary
 
 	function setName(funct: Function, value: any) {
 		funct(value)
 		inputRef.current.value = '' // returning the text inside input field to empty
 	}
+
+	// const handleAddCharacter = () => {
+	// 	const newItem = {
+	// 		itemName: 'i',
+	// 		quantity: 1,
+	// 		isSelected: false
+	// 	}
+
+	// 	const newItems = [...charList, newItem]
+
+	// 	setCharList(newItems)
+	// }
 
 	return (
 		user && (
@@ -56,13 +74,27 @@ const Profile = () => {
 				>
 					Confirm
 				</button>
-				<p>{user.email}</p>
+				<p style={{ fontWeight: 'bold' }}>{user.email}</p>
+				<div>
+					{charList.map((item: any) => (
+						<div key={item.name}>
+							<div>{item.name}</div>
+							<img
+								src={item.img}
+								alt="Character image"
+								height="100px"
+								width="100px"
+							/>
+							<div>{item.owner}</div>
+						</div>
+					))}
+				</div>
 				<button onClick={() => setCharacterEditIsShown(true)}>
 					Add character
 				</button>
 				<div>
 					{characterEditIsShown && (
-						<>
+						<div style={{ margin: '2vh' }}>
 							<label>
 								Set character name: ‎
 								<input
@@ -85,13 +117,22 @@ const Profile = () => {
 
 							<button
 								onClick={() => {
-									setCharacterEditIsShown(false)
 									setName(changeCharacterName, inputRef.current.value)
+									setCharList([
+										...charList,
+										{
+											name: characterName,
+											img: chars[charNum],
+											owner: user && user.name
+										}
+									])
+									changeCharNum(Math.floor(Math.random() * 4))
+									setCharacterEditIsShown(false)
 								}}
 							>
 								Submit
 							</button>
-						</>
+						</div>
 					)}
 				</div>
 			</div>
