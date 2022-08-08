396b0cc272c5a7e675074e8ff99f855bad881c07
MarinoLinic <marinolinic@gmail.com>
2022-08-08 13:30:00 +0200

    Added Radix UI and dropdown menu

--- README.md
+++ README.md
@@ -1,80 +1,33 @@
 # Naval Wars
 
-Install dependencies by typing
+## How to Run
 
-```
-npm i
-```
+Go into the frontend folder, run `npm i` and then `npm run dev`.
 
-or
+## Project Information
 
-```
-npm install
-```
+### Dependencies in the Frontend Folder
 
-into the terminal.
+- Next.js
+  - `npm install`
+  - `something`
+- Prisma
+  - `npm install`
+  - `command`
+- Tailwind CSS
+  - `npm install`
+  - `command`
+- Auth0
+  - `npm install`
+  - `command`
+- Radix UI
+  - `npm install @radix-ui/react-dropdown-menu`
 
---
+### Recommended VS Code extensions
 
-Run using
+- Prettier
+- Prisma (syntax highlighting)
+- Tailwind CSS IntelliSense
 
-```
-npm run dev
-```
-
-or
-
-```
-npx run dev
-```
-
----
-
-This is a [Next.js](https://nextjs.org/) project bootstrapped with
-[`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
-
-## Getting Started
-
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the
-result.
-
-You can start editing the page by modifying `pages/index.tsx`. The page
-auto-updates as you edit the file.
-
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on
-[http://localhost:3000/api/hello](http://localhost:3000/api/hello). This
-endpoint can be edited in `pages/api/hello.ts`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are
-treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead
-of React pages.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js
-  features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out
-[the Next.js GitHub repository](https://github.com/vercel/next.js/) - your
-feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the
-[Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme)
-from the creators of Next.js.
-
-Check out our
-[Next.js deployment documentation](https://nextjs.org/docs/deployment) for more
-details.
+- GitLens
+- Simple React Snippets
--- frontend/pages/navigation.tsx
+++ frontend/pages/navigation.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { useUser } from '@auth0/nextjs-auth0'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
 const Navigation = () => {
 	const { user, error, isLoading } = useUser()
@@ -43,77 +44,91 @@ const Navigation = () => {
 					)}
 				</ul>
 
-				<div className="flex items-center md:order-2">
-					<button
-						type="button"
-						className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-						id="user-menu-button"
-						aria-expanded="false"
-						data-dropdown-toggle="user-dropdown"
-						data-dropdown-placement="bottom">
-						<span className="sr-only">Open user menu</span>
-						<img className="w-8 h-8 rounded-full" src="/sailing_ship.png" alt="User photo" />
-					</button>
-					<div
-						className="hidden z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
-						id="user-dropdown">
-						<div className="py-3 px-4">
-							<span className="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
-							<span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">
-								your.name@navalwars.com
-							</span>
-						</div>
-						<ul className="py-1" aria-labelledby="user-menu-button">
-							<li>
-								<a
-									href="#"
-									className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-									Dashboard
-								</a>
-							</li>
-							<li>
-								<a
-									href="#"
-									className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-									Settings
-								</a>
-							</li>
-							<li>
-								<a
-									href="#"
-									className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-									Earnings
-								</a>
-							</li>
-							<li>
-								<a
-									href="#"
-									className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-									Sign out
-								</a>
-							</li>
-						</ul>
-					</div>
-					<button
-						data-collapse-toggle="mobile-menu-2"
-						type="button"
-						className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-						aria-controls="mobile-menu-2"
-						aria-expanded="false">
-						<span className="sr-only">Open main menu</span>
-						<svg
-							className="w-6 h-6"
-							aria-hidden="true"
-							fill="currentColor"
-							viewBox="0 0 20 20"
-							xmlns="http://www.w3.org/2000/svg">
-							<path
-								fill-rule="evenodd"
-								d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-								clip-rule="evenodd"></path>
-						</svg>
-					</button>
-				</div>
+				{/* Dropdown menu | https://codesandbox.io/s/xrxw8?file=/pages/index.tsx */}
+				<DropdownMenu.Root>
+					<DropdownMenu.Trigger>
+						<button
+							className="flex mr-8 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+							id="profile-menu-button">
+							<span className="sr-only">Open user menu</span>
+							<img className="w-8 h-8 rounded-full" src={user?.picture} alt="User photo" />
+						</button>
+					</DropdownMenu.Trigger>
+					{/* Portals the content into the body */}
+					<DropdownMenu.Portal>
+						<DropdownMenu.Content className="py-1 bg-black text-white flex items-center md:order-2">
+							<DropdownMenu.Label>{user?.name}</DropdownMenu.Label>
+							<DropdownMenu.Group className="">
+								{[
+									['Profile', '/profile'],
+									['Characters', '/profile'],
+									['Log out', '/api/auth/logout']
+								].map(([title, url]) => (
+									<Link href={url}>
+										<DropdownMenu.Item className="block py-2 px-4 text-sm cursor-pointer text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+											{title}
+										</DropdownMenu.Item>
+									</Link>
+								))}
+							</DropdownMenu.Group>
+						</DropdownMenu.Content>
+					</DropdownMenu.Portal>
+				</DropdownMenu.Root>
+
+				{/*
+				// <div className="flex items-center md:order-2">
+				// 	<button
+				// 		type="button"
+				// 		className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+				// 		id="user-menu-button"
+				// 		aria-expanded="false"
+				// 		data-dropdown-toggle="user-dropdown"
+				// 		data-dropdown-placement="bottom">
+				// 		<span className="sr-only">Open user menu</span>
+				// 		<img className="w-8 h-8 rounded-full" src="/sailing_ship.png" alt="User photo" />
+				// 	</button>
+				// 	<div
+				// 		className="z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
+				// 		id="user-dropdown">
+				// 		<div className="py-3 px-4">
+				// 			<span className="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
+				// 			<span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">
+				// 				your.name@navalwars.com
+				// 			</span>
+				// 		</div>
+				// 		<ul className="py-1" aria-labelledby="user-menu-button">
+				// 			{[
+				// 				['Profile', '/profile'],
+				// 				['Characters', '/profile'],
+				// 				['Log out', '/klemsen']
+				// 			].map(([title, url]) => (
+				// 				<li className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+				// 					<Link href={url}>{title}</Link>
+				// 				</li>
+				// 			))}
+				// 		</ul>
+				// 	</div>
+				// 	<button
+				// 		data-collapse-toggle="mobile-menu-2"
+				// 		type="button"
+				// 		className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+				// 		aria-controls="mobile-menu-2"
+				// 		aria-expanded="false">
+				// 		<span className="sr-only">Open main menu</span>
+				// 		<svg
+				// 			className="w-6 h-6"
+				// 			aria-hidden="true"
+				// 			fill="currentColor"
+				// 			viewBox="0 0 20 20"
+				// 			xmlns="http://www.w3.org/2000/svg">
+				// 			<path
+				// 				fill-rule="evenodd"
+				// 				d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+				// 				clip-rule="evenodd"></path>
+				// 		</svg>
+				// 	</button>
+				// </div>
+		*/}
 			</div>
 		</nav>
 	)
