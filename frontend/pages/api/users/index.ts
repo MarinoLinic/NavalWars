e6c90af0be6fc752a98387657ca44a82e0160b4f
Kalciphoz <Cornelius.dybdahl@gmail.com>
2022-08-14 18:24:24 +0200

    minor changes

--- frontend/pages/api/users/index.ts
+++ frontend/pages/api/users/index.ts
@@ -1,12 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient, Prisma } from '@prisma/client'
+import { ServerResponse } from 'http'
 
 const prisma = new PrismaClient()
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	if (req.method === 'GET') {
 		const users = await prisma.user.findMany()
-		return res.send(users)
+			return res.send(users)
 	} else if (req.method === 'POST') {
 		// const result = await prisma.user.create({
 		// 	data: {
@@ -15,7 +16,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 		// })
 		// res.json(result)
 
-		const { body: data } = req
+		//const { body: data } = req
+		const data = req.body;
 		const newUser = await prisma.user.create({ data })
 		// We're missing server-side validation
 		return res.status(201).send(newUser)
