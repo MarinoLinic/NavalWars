95812041d03790ba4a15f8e0cbadc6ccb1ed8587
MarinoLinic <84214511+MarinoLinic@users.noreply.github.com>
2022-08-19 06:16:20 +0200

    A little reordering

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Navigation from './navigation'
+import Navigation from '../components/navigation'
 import { UserProvider } from '@auth0/nextjs-auth0'
 
 function MyApp({ Component, pageProps }: AppProps) {
--- frontend/pages/klemsen.tsx
+++ frontend/pages/klemsen.tsx
@@ -1,19 +1,31 @@
 import data from '../components/data.json'
 import * as React from 'react'
 import { useState } from 'react'
+import { Prisma, PrismaClient } from '@prisma/client'
+
+let prisma = new PrismaClient()
+
+const create = async () => {
+	fetch('http://localhost:3000/api/users/create', {
+		body: JSON.stringify(data),
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	})
+}
+
+export const getServerSideProps = async () => {
+	const users = await prisma.user.findMany()
+	return {
+		props: {
+			data
+		}
+	}
+}
 
 function Cornelius() {
 	const [klemens, setklemens] = useState('klemens')
-
-	const create = async () => {
-		fetch('http://localhost:3000/api/users/create', {
-			body: JSON.stringify(data),
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		})
-	}
 
 	return (
 		<>
