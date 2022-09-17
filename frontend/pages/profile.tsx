fcf53e88e9d3914e4b099cd385d49b70337640e7
MarinoLinic <84214511+MarinoLinic@users.noreply.github.com>
2022-09-17 23:42:14 +0200

    Separated tasks from README

--- frontend/pages/profile.tsx
+++ frontend/pages/profile.tsx
@@ -32,60 +32,58 @@ function Profile() {
 
   return (
     /* User profile */
-    session.status === "authenticated" && (
-      <div className="flex flex-col items-center my-16">
-        <img
-          src={profilePicture}
-          alt={"Profile image"}
-          className="w-36 mb-4 rounded-full ring-8 ring-gray-200"
-          referrerPolicy="no-referrer"
-        />
+    <div className="flex flex-col items-center my-16">
+      <img
+        src={profilePicture}
+        alt={"Profile image"}
+        className="w-36 mb-4 rounded-full ring-8 ring-gray-200"
+        referrerPolicy="no-referrer"
+      />
 
-        <h2 className="text-2xl font-bold text-gray-700 mb-4">
-          {
-            /* if the frontend variable is empty after reload, fetch from db */
-            profileName || session.data?.user?.name
-          }
-        </h2>
+      <h2 className="text-2xl font-bold text-gray-700 mb-4">
+        {
+          /* if the frontend variable is empty after reload, fetch from db */
+          profileName || session.data?.user?.name
+        }
+      </h2>
 
-        <p className="text-xl font-semibold text-gray-700 mb-4">
-          E-mail: {session.data?.user?.email}
-        </p>
+      <p className="text-xl font-semibold text-gray-700 mb-4">
+        E-mail: {session.data?.user?.email}
+      </p>
 
-        <button className="mb-4" onClick={() => setEditProfile(true)}>
-          Edit profile
-        </button>
+      <button className="mb-4" onClick={() => setEditProfile(true)}>
+        Edit profile
+      </button>
 
-        {editProfile && (
-          <>
-            <label>
-              Change name:
-              <input
-                ref={inputRef}
-                type="text"
-                id="user-name"
-                name="User name"
-                autoComplete="off"
-                placeholder="Write here..."
-              />
-            </label>
+      {editProfile && (
+        <>
+          <label>
+            Change name:
+            <input
+              ref={inputRef}
+              type="text"
+              id="user-name"
+              name="User name"
+              autoComplete="off"
+              placeholder="Write here..."
+            />
+          </label>
 
-            <button
-              onClick={() => {
-                http_fetch.post("users/change_name", {
-                  name: inputRef.current.value,
-                  email: session.data?.user?.email,
-                });
-                setName(setProfileName, inputRef.current.value);
-                setEditProfile(false);
-              }}
-            >
-              Confirm
-            </button>
-          </>
-        )}
-      </div>
-    )
+          <button
+            onClick={() => {
+              http_fetch.post("users/change_name", {
+                name: inputRef.current.value,
+                email: session.data?.user?.email,
+              });
+              setName(setProfileName, inputRef.current.value);
+              setEditProfile(false);
+            }}
+          >
+            Confirm
+          </button>
+        </>
+      )}
+    </div>
   );
 }
 
