1e2f0a022aadd15ab8342665d3b3ef3d0948a5c2
MarinoLinic <84214511+MarinoLinic@users.noreply.github.com>
2022-09-20 23:22:21 +0200

    added template for Tailwind config

---
+++ frontend/prisma/migrations/20220917214817_marino_sync/migration.sql
@@ -0,0 +1,2 @@
+-- DropIndex
+DROP INDEX `Task_Naval_destination_fkey` ON `task_naval`;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		extend: {}
-	},
-	plugins: []
-}
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        tertiary: "#00f6ff",
+      },
+      fontFamily: {
+        // poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
